258f3c11aa05d158734ac10d6bce93fdb4893f6b
fiehtle <viet.dt.le@gmail.com>
2022-01-23 12:26:35 +0100

    hid env variables

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,40 +1,64 @@
-import Seo from '@/components/Seo';
-import CustomLink from '@/components/CustomLink';
+import TrackList from '@/components/track-list';
+import { useEffect, useState } from 'react';
+
+const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+const REDIRECT_URI = 'http://localhost:3000';
+const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
+const RESPONSE_TYPE = 'token';
+const TOP_TRACKS_ENDPOINT = `https://api.spotify.com/v1/me/top/tracks?time_range=long_term`;
 
 export default function Home() {
+  const [token, setToken] = useState('');
+  const [tracks, setTracks] = useState([]);
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem('token');
+
+    if (!token && hash) {
+      token = hash
+        .substring(1)
+        .split('&')
+        .find((elem) => elem.startsWith('access_token'))
+        .split('=')[1];
+
+      window.location.hash = '';
+      window.localStorage.setItem('token', token);
+    }
+    setToken(token);
+  }, []);
+
+  const logout = () => {
+    setToken('');
+    window.localStorage.removeItem('token');
+  };
+
+  const getTopTracks = async (e) => {
+    e.preventDefault();
+
+    //let { access_token } = await getAccessToken();
+
+    const { data } = await fetch(TOP_TRACKS_ENDPOINT, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    setTracks(data);
+    console.log(tracks);
+  };
   return (
     <>
-      <Seo templateTitle='Home' />
-
-      <main>
-        <section className='bg-dark'>
-          <div className='flex flex-col items-center justify-center min-h-screen text-white layout'>
-            <h1>
-              <CustomLink href='https://github.com/theodorusclarence/nextjs-tailwind-starter'>
-                NextJS Tailwind Starter
-              </CustomLink>
-            </h1>
-            <p className='mb-4'></p>
-            <a
-              target='_blank'
-              rel='noopener noreferrer'
-              href='https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Ftheodorusclarence%2Fnextjs-tailwind-starter'
-            >
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img src='https://vercel.com/button' alt='Deploy with Vercel' />
-            </a>
-            <div className='mt-8 text-dark'>
-              <p className='text-[#ffe347]'>JIT is on</p>
-            </div>
-            <footer className='absolute text-gray-500 bottom-2'>
-              © {new Date().getFullYear()} By{' '}
-              <CustomLink href='https://theodorusclarence.com?ref=nextstarter'>
-                Theodorus Clarence
-              </CustomLink>
-            </footer>
-          </div>
-        </section>
-      </main>
+      {!token ? (
+        <a
+          href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+        >
+          Login to Spotify
+        </a>
+      ) : (
+        <button onClick={logout}>Logout</button>
+      )}
+      <TrackList />
+      <button onClick={getTopTracks}>Get tracks</button>
     </>
   );
 }
